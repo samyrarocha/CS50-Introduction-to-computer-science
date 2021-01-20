c35efe30fc1e9225e7516ecc0eb458dc527a531f
samyrarocha <samyrarocha@users.noreply.github.com>
2021-01-20 02:12:50 +0000

    automated commit by submit50

--- population.c
+++ population.c
@@ -7,21 +7,21 @@ int main(void)
 
     int start = get_int("Start Size: ");
     while (start < 9)
-        {
+    {
         printf("It should be greater than 9!\n");
         start = get_int("Start Size: ");
 
-        }
+    }
 
     // TODO: Prompt for end size
 
     int end = get_int("End Size: ");
-        while (end < start)
-            {
-            printf("Just kill %i llama(s). You should try a number greater than the previous one.\n", start - end);
-            end = get_int("End Size: ");
+    while (end < start)
+    {
+        printf("Just kill %i llama(s). You should try a number greater than the previous one.\n", start - end);
+        end = get_int("End Size: ");
 
-        }
+    }
     // TODO: Calculate number of years until we reach threshold
 
     int gain, loss, total;
@@ -31,10 +31,10 @@ int main(void)
     while (total < end)
     {
 
-       gain = (float) total / (float) 3;
-       loss = (float) total / (float) 4;
-       total = total + gain - loss;
-       n++;
+        gain = (float) total / (float) 3;
+        loss = (float) total / (float) 4;
+        total = total + gain - loss;
+        n++;
 
     }
 
