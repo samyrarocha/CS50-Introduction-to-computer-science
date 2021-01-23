4e3d4ce00f301fa27e0d5cc2be7f8e354a418b98
samyrarocha <samyrarocha@users.noreply.github.com>
2021-01-23 19:13:58 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -16,25 +16,30 @@ int main(void)
     int cents = round(dollar * 100);
     int coins;
     coins = 0;
+    //Quaters
     while (cents >= 25)
     {
         coins = coins + 1;
         cents = cents - 25;
     }
+    //Dimes
     while (cents >= 10)
     {
         coins = coins + 1;
         cents = cents - 10;
     }
+    //Nickels
     while (cents >= 5)
     {
         coins = coins + 1;
         cents = cents - 5;
     }
+    //Pennies
     while (cents >= 1)
     {
         coins = coins + 1;
         cents = cents - 1;
     }
+    //Printing number of coins
     printf("Coins: %i\n", coins);
 }
