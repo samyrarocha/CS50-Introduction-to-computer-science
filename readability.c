2f79b56bbbfcbe14cf40ca0ee3f805681e87a764
samyrarocha <samyrarocha@users.noreply.github.com>
2021-02-06 23:27:57 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -11,24 +11,25 @@ int main(void)
 {
     //Asking for the text
     string text = get_string("Text: ");
-    
+
     //Grade of the text
     float L = count_letters(text);
     float S = count_sentences(text);
     float index = (0.0588 * L) - (0.296 * S) - 15.8;
-    
+    int seila = round(index);
+
     //Print grade
-    if (round(index) < 1)
+    if (seila < 1)
     {
         printf("Before Grade 1\n");
     }
-    else if (round(index) > 16)
+    else if (seila > 16)
     {
         printf("Grade 16+\n");
     }
     else
     {
-        printf("Grade %g\n", floor(index));
+        printf("Grade %d\n", seila);
     }
 }
 
@@ -44,7 +45,7 @@ int count_letters(string text)
         //Counting letters
         if (isalpha(text[i]) != 0)
         {
-          letters++;  
+          letters++;
         }
         //Counting words
         if (isspace(text[i]) != 0)
